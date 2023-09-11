16b146d8dd4c42e9966f6da9c3515091ad811c2c
OkechEdu <okechkazi@outlook.com>
2023-09-11 05:32:34 +0300

    addition of the site's config name and description

--- config/site.ts
+++ config/site.ts
@@ -1,8 +1,8 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "Name",
-  description: "Description",
+  name: "NajCollections",
+  description: "Your one stop shop for all your clothing needs in Kenya",
   footer: [
     { name: "Home", href: "/" },
     { name: "About", href: "#" },
