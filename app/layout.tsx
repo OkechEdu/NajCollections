55d896c86945c85c331e676800909c895b3aecc0
OkechEdu <okechkazi@outlook.com>
2023-09-11 05:31:26 +0300

    added default layout provider and site header, blod and footer components

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,13 @@ import { SiteBlob } from "@/components/site-blob"
 import { SiteFooter } from "@/components/site-footer"
 import { SiteHeader } from "@/components/site-header"
 
-export const metadata: Metadata = {}
+export const metadata: Metadata = {
+  title: siteConfig.name,
+  description: siteConfig.description,
+  icons: {
+    icon: '/favicon.ico'
+  }
+}
 
 interface RootLayoutProps {
   children: React.ReactNode
@@ -26,9 +32,19 @@ export default function RootLayout({ children }: RootLayoutProps) {
             fontSans.variable
           )}
         >
+          <Providers> 
+            {/* providers are important for react context providers components to work properly
+            this why we wrap the page with the default provider. */}
           <div className="relative flex min-h-screen flex-col">
+            {/* site header content */}
+            <SiteHeader />
+            {/* site bloob content */}
+            <SiteBlob />
             <div className="flex-1">{children}</div>
+            {/* site footer */}
+            <SiteFooter />
           </div>
+          </Providers>
         </body>
       </html>
     </>
